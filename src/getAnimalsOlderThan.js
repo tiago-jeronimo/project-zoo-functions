4f6ad8048b0b66802e08e537b49f634cf7395c73
Tiago Jerônimo <tiago.batmansz@gmail.com>
2022-02-10 18:26:54 -0300

    Requisito 1 concertado e Requisito 2 feito

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const verificationAnimal = species.find((element) => element.name === animal);
+  const { residents } = verificationAnimal;
+  return residents.every((elemento) => elemento.age > age);
 }
 
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,8 +1,9 @@
-const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function getSpeciesByIds(...ids) {
-  return ids.map((id) => species.find((species)=>species.id ===id)); 
+  return ids.map((id) => species.find((element) => element.id === id));
 }
 
 module.exports = getSpeciesByIds;
