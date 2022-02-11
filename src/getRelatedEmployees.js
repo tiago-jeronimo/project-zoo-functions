25b9bc6dbe8186817fee66dd04960b0846f96947
Tiago Jerônimo <tiago.batmansz@gmail.com>
2022-02-11 16:35:01 -0300

    Requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,16 @@
 const data = require('../data/zoo_data');
 
+// isManager é uma função que tem como parametro um 'id', o comportamento da função é ir até a seção employees, depois se o resultado da propriedade managers dessa seção é igual ao id repassado por parâmetro. True se sim, false se não.
 function isManager(id) {
-  // seu código aqui
+  return data.employees.some((elementoEmsi) => elementoEmsi.managers.includes(id));
 }
-
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)) {
+    return data.employees.filter((element) => element.managers.includes(managerId)) // retorna um novo array com os dados do gerente
+      .map((elements) => `${elements.firstName} ${elements.lastName}`); // o map ira fazer a execução da função para cada elements, nesse caso, irá criar um array com o nome e sobrenome.
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
+// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/includes#sum%C3%A1rio
