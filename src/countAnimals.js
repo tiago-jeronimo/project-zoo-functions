1217e8a257ced725c06ed5e16cf667007c1fab22
Tiago Jerônimo <tiago.batmansz@gmail.com>
2022-02-12 01:47:16 -0300

    Requisito 5 - feito com a ajuda de Bruno Monteiro

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,11 +1,22 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-//   if (countAnimals(undefined)) {
-//     const objectReturn = {};
-//     data.species.forEach((elemento, indice)
-//   }
+  if (!animal) {
+    const object = {};
+    data.species.forEach((element) => { object[element.name] = element.residents.length; });
+    return object;
+  }
+  if (Object.values(animal).length === 1) {
+    const nameAnimal = Object.values(animal);
+    const buscaObjeto = data.species.find((element) => element.name === nameAnimal[0]);
+    return buscaObjeto.residents.length;
+  }
+  if (Object.values(animal).length === 2) {
+    const nameAnimal = Object.values(animal);
+    const buscaObjeto = data.species.find((element) => element.name === nameAnimal[0]);
+    const filtroSex = buscaObjeto.residents.filter((element) => element.sex === animal.sex);
+    return filtroSex.length;
+  }
 }
-// console.log(data.species.name);
 
 module.exports = countAnimals;
