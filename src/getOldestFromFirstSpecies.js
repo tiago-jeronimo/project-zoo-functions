3c80ee2cd884e5a8adadae48c73c4f31d3d47ed3
Tiago Jerônimo <tiago.batmansz@gmail.com>
2022-02-12 00:57:50 -0300

    Requisito 9 feito - com ajuda de Sandro Bistene e Bruno Monteiro

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const idAnimal = data.employees.find((elemento) => elemento.id === id).responsibleFor[0];
+  const buscaAnimal = data.species.find((element) => element.id === idAnimal);
+  let result = buscaAnimal.residents[0];
+  buscaAnimal.residents.forEach((elemento) => {
+    if (elemento.age > result.age) {
+      result = elemento;
+    }
+  });
+  return Object.values(result);
 }
 
 module.exports = getOldestFromFirstSpecies;
