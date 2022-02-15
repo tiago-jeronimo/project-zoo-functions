9e08ad45f4337a4e945d5bd9522390f73391ec7c
Tiago Jerônimo <tiago.batmansz@gmail.com>
2022-02-14 21:24:12 -0300

    Requisito 10 feito, com a ajuda de Bruno Monteiro e do Sandro Bistene, imensamente grato

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,43 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const { employees } = data;
+const { species } = data;
+
+const saida = employees.map((elemento) => {
+  const empregado = {
+    id: elemento.id,
+    fullName: `${elemento.firstName} ${elemento.lastName}`,
+    species: species.filter((specie) => elemento.responsibleFor.includes(specie.id))
+      .map((nome) => nome.name),
+    locations: species.filter((specie) => elemento.responsibleFor.includes(specie.id))
+      .map((nome) => nome.location),
+  };
+  return empregado;
+});
+function checkEmployees(name, input) {
+  let recebe = false;
+  employees.forEach((element) => {
+    const { firstName, lastName, id } = element;
+    if (firstName === name || lastName === name || id === input) {
+      recebe = true;
+    }
+  });
+  return recebe;
 }
 
+function getEmployeesCoverage(param) {
+  if (!param) {
+    return saida;
+  }
+  const { name, id } = param;
+  if (!checkEmployees(name, id)) {
+    throw new Error('Informações inválidas');
+  }
+  if (Object.keys(param).includes('id')) {
+    const saidaRequisito = saida.find((element) => element.id.includes(param.id));
+    return saidaRequisito;
+  }
+  // if (Object.keys(param).includes('name')) {
+  return saida.find((element) => element.fullName.includes(param.name));
+}
 module.exports = getEmployeesCoverage;
